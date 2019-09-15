9d0a141fe85b8b0927de56d01188135f564cecea
Isaac Dorfman <isaacdorfman@Isaacs-MBP.fios-router.home>
2019-09-14 20:20:07 -0400

    mostly working

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-#DCDAT35 Midcourse Project
-#Isaac Dorfman
-#September 7, 2019
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
 import pandas as pd
-import os
+import plotly.graph_objs as go
+
 from pathlib import Path
 
-#import and prepare the data for mapping
+# Step 0. Set up informational variables
+sourceurl = 'https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016'
+githublink = 'https://github.com/ihdorfman/DCDAT35midcourse'
+tabtitle = 'Global Mental Health'
+
+# Step 1. Launch the application
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+# Step 2. Import the dataset
 home = Path.cwd()
 
 filepath = Path.joinpath(home, 'data', 'master.csv')
@@ -29,32 +39,84 @@ df4 = df3[['country','year','per100','locations']]
 
 df5 = df4.groupby(['country','locations','year']).agg(sum).reset_index()
 
-#Set up for information about ther data set used and github repository
-mygraphtitle = 'Suicide Rates by Country by Year'
-mycolorscale = 'plasma' # Note: The error message will list possible color scales.
-mycolorbartitle = "Suicides per 100k"
-tabtitle = 'Global Mental Health'
-sourceurl = 'https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016'
-githublink = 'https://github.com/ihdorfman/DCDAT35midcourse'
+
+# year slider options
+YEARS = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, \
+            1994, 1995, 1996, 1997, 1998, 1999,2000, 2001, 2002,\
+            2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, \
+            2012, 2013, 2014, 2015, 2016]
 
 
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
+# Step 3. Create a plotly figure
+trace_1 = go.Choropleth(
+            locations=df5[df5['year']==1985]['country'], # Spatial coordinates
+            z = df5[df5['year']==1985]['per100'].astype(float), # Data to be color-coded
+            locationmode = 'country names', # set of locations match entries in `locations`
+            colorscale = 'Blues',
+            colorbar_title = 'Suicides per 100K')
+
+fig = go.Figure(data = [trace_1])
+
+fig.update_layout(
+    geo_scope='world',
+    width=1200,
+    height=800
+)
 
 
-#MAIN APP GUTS GO HERE!!!!
+# Step 4. Create a Dash layout
+app.layout = html.Div([
+                # a header and a paragraph
+                html.Div([
+                    html.H4('Suicide Rate by Country per 100K Residents 1985-2016'),
+                    html.P("Isaac Dorfman - DCDAT35 Midcourse Project")
+                         ],
+                     style = {'padding' : '15px' ,
+                              'backgroundColor' : '#8DA1A0'}),
+                # range slider
+                html.P([
+                    html.Label("Year:"),
+                    dcc.Slider(id='slider',
+					               min=min(YEARS),
+                                   max=max(YEARS),
+                                   value=min(YEARS),
+                                   marks={str(year): str(year) for year in YEARS},),
+			            ], style={'width':1000, 'margin':25}),
+
+                # adding a plot
+                dcc.Graph(id = 'plot', figure = fig),
 
 
+                    html.A('Code on Github', href=githublink),
+                    html.Br(),
+                    html.A("Data Source", href=sourceurl)
 
 
+                      ])
 
 
+# Step 5. Add callback functions
+@app.callback(Output('plot', 'figure'),
+             [Input('slider', 'value')])
+def update_figure(input):
 
+    # updating the plot
+    trace_1 = go.Choropleth(
+                locations=df5[df5['year']==input]['country'], # Spatial coordinates
+                z = df5[df5['year']==input]['per100'].astype(float), # Data to be color-coded
+                locationmode = 'country names', # set of locations match entries in `locations`
+                colorscale = 'Blues',
+                colorbar_title = 'Suicides per 100K')
 
+    fig = go.Figure(data = [trace_1])
 
-############ Deploy
+    fig.update_layout(
+        geo_scope='world',
+        width=1200,
+        height=800
+    )
+    return fig
+
+# Step 6. Add the server clause
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug = True)
